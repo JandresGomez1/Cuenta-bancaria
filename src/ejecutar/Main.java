c8e034a6971b097ba6894a8e19ba48d99c82bcc6
james <jamesagomez2004@gmail.com>
2024-10-11 17:50:29 -0500

    Proyecto finalizado

---
+++ src/ejecutar/Main.java
@@ -0,0 +1,16 @@
+package ejecutar;
+
+import cuentabancaria.CuentaAhorro;
+
+public class Main {
+    
+    public static void main(String[] args) {
+       CuentaAhorro cuenta1 = new CuentaAhorro(100000, 0.10, true);
+        cuenta1.imprimir();
+        cuenta1.consignar(50000);
+        cuenta1.retirar(70000);
+        cuenta1.calcularInteres();
+        cuenta1.imprimirF();
+    }
+    
+}
